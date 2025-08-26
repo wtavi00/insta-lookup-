15e484f42030cdb602fea581d86bf681c5625b28
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-26 09:03:12 +0600

    Update Tool.py

--- Tool.py
+++ Tool.py
@@ -26,3 +26,33 @@ def get_user_id(username, sessionid):
         return {"id": None, "error": "Rate limit or bad response"}
 
 
+def get_info(search, sessionid, search_type="username"):
+    if search_type == "username":
+        data = get_user_id(search, sessionid)
+        if data["error"]:
+            return {"user": None, "error": data["error"]}
+        user_id = data["id"]
+    else:
+        try:
+            user_id = str(int(search))
+        except ValueError:
+            return {"user": None, "error": "Invalid ID"}
+    try:
+        response = requests.get(
+            f'https://i.instagram.com/api/v1/users/{user_id}/info/',
+            headers={'User-Agent': 'Instagram 64.0.0.14.96'},
+            cookies={'sessionid': sessionid}
+        )
+        if response.status_code == 429:
+            return {"user": None, "error": "Rate limit exceeded"}
+
+        response.raise_for_status()
+        user_data = response.json().get("user")
+        if not user_data:
+            return {"user": None, "error": "User not found"}
+        user_data["userID"] = user_id
+        return {"user": user_data, "error": None}
+    except requests.exceptions.RequestException:
+        return {"user": None, "error": "Request failed"}
+
+
